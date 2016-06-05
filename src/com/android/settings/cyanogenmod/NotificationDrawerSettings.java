49a176989e72849e401e6bab285a8d46984d3a88
qjohn <townex22@gmail.com>
2016-06-06 03:07:58 +0700

    Settings:Add quick settings animation[2/2]

--- src/com/android/settings/cyanogenmod/NotificationDrawerSettings.java
+++ src/com/android/settings/cyanogenmod/NotificationDrawerSettings.java
@@ -48,6 +48,9 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment imple
     private static final String PREF_BLOCK_ON_SECURE_KEYGUARD = "block_on_secure_keyguard";
     private static final String PREF_ENABLE_TASK_MANAGER = "enable_task_manager";
     private static final String PREF_STATUS_BAR_HEADER_FONT_STYLE = "status_bar_header_font_style";
+    private static final String PREF_TILE_ANIM_STYLE = "qs_tile_animation_style";
+    private static final String PREF_TILE_ANIM_DURATION = "qs_tile_animation_duration";
+    private static final String PREF_TILE_ANIM_INTERPOLATOR = "qs_tile_animation_interpolator";
 
     private ListPreference mQuickPulldown;
     private ListPreference mSmartPulldown;
@@ -56,6 +59,9 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment imple
     private Preference mQSTiles;
     private ListPreference mNumColumns;
     private ListPreference mStatusBarHeaderFontStyle;
+    private ListPreference mTileAnimationStyle;
+    private ListPreference mTileAnimationDuration;
+    private ListPreference mTileAnimationInterpolator;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -88,6 +94,31 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment imple
         } else {
             prefSet.removePreference(mBlockOnSecureKeyguard);
         }
+
+        mTileAnimationStyle = (ListPreference) findPreference(PREF_TILE_ANIM_STYLE);
+        int tileAnimationStyle = Settings.System.getIntForUser(resolver,
+                Settings.System.ANIM_TILE_STYLE, 0,
+                UserHandle.USER_CURRENT);
+        mTileAnimationStyle.setValue(String.valueOf(tileAnimationStyle));
+        updateTileAnimationStyleSummary(tileAnimationStyle);
+        updateAnimTileStyle(tileAnimationStyle);
+        mTileAnimationStyle.setOnPreferenceChangeListener(this);
+ 
+        mTileAnimationDuration = (ListPreference) findPreference(PREF_TILE_ANIM_DURATION);
+        int tileAnimationDuration = Settings.System.getIntForUser(resolver,
+                Settings.System.ANIM_TILE_DURATION, 1500,
+                UserHandle.USER_CURRENT);
+        mTileAnimationDuration.setValue(String.valueOf(tileAnimationDuration));
+        updateTileAnimationDurationSummary(tileAnimationDuration);
+        mTileAnimationDuration.setOnPreferenceChangeListener(this);
+         
+        mTileAnimationInterpolator = (ListPreference) findPreference(PREF_TILE_ANIM_INTERPOLATOR);
+        int tileAnimationInterpolator = Settings.System.getIntForUser(resolver,
+                Settings.System.ANIM_TILE_INTERPOLATOR, 0,
+                UserHandle.USER_CURRENT);
+        mTileAnimationInterpolator.setValue(String.valueOf(tileAnimationInterpolator));
+        updateTileAnimationInterpolatorSummary(tileAnimationInterpolator);
+        mTileAnimationInterpolator.setOnPreferenceChangeListener(this);
 
         // Smart Pulldown
         mSmartPulldown = (ListPreference) findPreference(PREF_SMART_PULLDOWN);
@@ -141,6 +172,25 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment imple
             Settings.Secure.putInt(getContentResolver(),
                     Settings.Secure.STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD,
                     (Boolean) newValue ? 1 : 0);
+            return true;
+        } else if (preference == mTileAnimationStyle) {
+             int tileAnimationStyle = Integer.valueOf((String) newValue);
+             Settings.System.putIntForUser(resolver, Settings.System.ANIM_TILE_STYLE,
+                     tileAnimationStyle, UserHandle.USER_CURRENT);
+             updateTileAnimationStyleSummary(tileAnimationStyle);
+             updateAnimTileStyle(tileAnimationStyle);
+             return true;
+         } else if (preference == mTileAnimationDuration) {
+             int tileAnimationDuration = Integer.valueOf((String) newValue);
+             Settings.System.putIntForUser(resolver, Settings.System.ANIM_TILE_DURATION,
+                     tileAnimationDuration, UserHandle.USER_CURRENT);
+             updateTileAnimationDurationSummary(tileAnimationDuration);
+             return true;
+         } else if (preference == mTileAnimationInterpolator) {
+            int tileAnimationInterpolator = Integer.valueOf((String) newValue);
+            Settings.System.putIntForUser(resolver, Settings.System.ANIM_TILE_INTERPOLATOR,
+                    tileAnimationInterpolator, UserHandle.USER_CURRENT);
+            updateTileAnimationInterpolatorSummary(tileAnimationInterpolator);
             return true;
         } else if (preference == mSmartPulldown) {
             int smartPulldown = Integer.valueOf((String) newValue);
@@ -220,6 +270,36 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment imple
                 .valueOf(numColumns))];
         mNumColumns.setSummary(getResources().getString(R.string.qs_num_columns_showing, prefix));
     }
+
+    private void updateTileAnimationStyleSummary(int tileAnimationStyle) {
+         String prefix = (String) mTileAnimationStyle.getEntries()[mTileAnimationStyle.findIndexOfValue(String
+                 .valueOf(tileAnimationStyle))];
+         mTileAnimationStyle.setSummary(getResources().getString(R.string.qs_set_animation_style, prefix));
+    }
+
+    private void updateTileAnimationDurationSummary(int tileAnimationDuration) {
+         String prefix = (String) mTileAnimationDuration.getEntries()[mTileAnimationDuration.findIndexOfValue(String
+                 .valueOf(tileAnimationDuration))];
+         mTileAnimationDuration.setSummary(getResources().getString(R.string.qs_set_animation_duration, prefix));
+    }
+     
+    private void updateTileAnimationInterpolatorSummary(int tileAnimationInterpolator) {
+        String prefix = (String) mTileAnimationInterpolator.getEntries()[mTileAnimationInterpolator.findIndexOfValue(String
+                .valueOf(tileAnimationInterpolator))];
+        mTileAnimationInterpolator.setSummary(getResources().getString(R.string.qs_set_animation_interpolator, prefix));
+    }
+ 
+    private void updateAnimTileStyle(int tileAnimationStyle) {
+         if (mTileAnimationDuration != null) {
+             if (tileAnimationStyle == 0) {
+                 mTileAnimationDuration.setSelectable(false);
+                 mTileAnimationInterpolator.setSelectable(false);
+             } else {
+                 mTileAnimationDuration.setSelectable(true);
+                 mTileAnimationInterpolator.setSelectable(true);
+             }
+         }
+     }
 
     private int getDefaultNumColums() {
         try {
