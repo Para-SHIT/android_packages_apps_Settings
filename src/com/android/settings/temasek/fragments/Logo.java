a3cd0be359a71ea8064eb10550d2e917b8be1025
qjohn <townex22@gmail.com>
2016-06-12 06:39:29 +0700

    settings:Custom logo & revert Wi-Fi SSID[1/2]

--- src/com/android/settings/temasek/fragments/Logo.java
+++ src/com/android/settings/temasek/fragments/Logo.java
@@ -29,6 +29,7 @@ import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -40,10 +41,14 @@ public class Logo extends SettingsPreferenceFragment implements OnPreferenceChan
 
     public static final String TAG = "Logo";
 
+    private static final String KEY_CUSTOM_LOGO_COLOR = "custom_logo_color";
+    private static final String KEY_CUSTOM_LOGO_STYLE = "custom_logo_style";
     private static final String KEY_TEMASEK_LOGO_COLOR = "status_bar_temasek_logo_color";
     private static final String KEY_TEMASEK_LOGO_STYLE = "status_bar_temasek_logo_style";
 
+    private ColorPickerPreference mCustomLogoColor;
     private ColorPickerPreference mTemasekLogoColor;
+    private ListPreference mCustomLogoStyle;
     private ListPreference mTemasekLogoStyle;
 
     @Override
@@ -54,14 +59,16 @@ public class Logo extends SettingsPreferenceFragment implements OnPreferenceChan
 
         ContentResolver resolver = getActivity().getContentResolver();
         PreferenceScreen prefSet = getPreferenceScreen();
+        int intColor;
+        String hexColor;
 
         // Temasek logo color
         mTemasekLogoColor =
             (ColorPickerPreference) prefSet.findPreference(KEY_TEMASEK_LOGO_COLOR);
         mTemasekLogoColor.setOnPreferenceChangeListener(this);
-        int intColor = Settings.System.getInt(resolver,
+        intColor = Settings.System.getInt(resolver,
             Settings.System.STATUS_BAR_TEMASEK_LOGO_COLOR, 0xffffffff);
-        String hexColor = String.format("#%08x", (0xffffffff & intColor));
+        hexColor = String.format("#%08x", (0xffffffff & intColor));
         mTemasekLogoColor.setSummary(hexColor);
         mTemasekLogoColor.setNewPreviewColor(intColor);
 
@@ -72,6 +79,24 @@ public class Logo extends SettingsPreferenceFragment implements OnPreferenceChan
         mTemasekLogoStyle.setValue(String.valueOf(temasekLogoStyle));
         mTemasekLogoStyle.setSummary(mTemasekLogoStyle.getEntry());
         mTemasekLogoStyle.setOnPreferenceChangeListener(this);
+
+        // custom logo color
+        mCustomLogoColor =
+            (ColorPickerPreference) prefSet.findPreference(KEY_CUSTOM_LOGO_COLOR);
+        mCustomLogoColor.setOnPreferenceChangeListener(this);
+        intColor = Settings.System.getInt(getContentResolver(),
+                Settings.System.CUSTOM_LOGO_COLOR, 0xffffffff);
+        hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mCustomLogoColor.setSummary(hexColor);
+        mCustomLogoColor.setNewPreviewColor(intColor);
+
+        mCustomLogoStyle = (ListPreference) findPreference(KEY_CUSTOM_LOGO_STYLE);
+        int customLogoStyle = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.CUSTOM_LOGO_STYLE, 0,
+                UserHandle.USER_CURRENT);
+        mCustomLogoStyle.setValue(String.valueOf(customLogoStyle));
+        mCustomLogoStyle.setSummary(mCustomLogoStyle.getEntry());
+        mCustomLogoStyle.setOnPreferenceChangeListener(this);
 
     }
 
@@ -94,6 +119,23 @@ public class Logo extends SettingsPreferenceFragment implements OnPreferenceChan
                     UserHandle.USER_CURRENT);
             mTemasekLogoStyle.setSummary(
                     mTemasekLogoStyle.getEntries()[index]);
+            return true;
+        } else if (preference == mCustomLogoColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.CUSTOM_LOGO_COLOR, intHex);
+            return true;
+        } else if (preference == mCustomLogoStyle) {
+            int customLogoStyle = Integer.valueOf((String) newValue);
+            int index = mCustomLogoStyle.findIndexOfValue((String) newValue);
+            Settings.System.putIntForUser(
+                   getContentResolver(), Settings.System.CUSTOM_LOGO_STYLE, customLogoStyle,
+                   UserHandle.USER_CURRENT);
+            mCustomLogoStyle.setSummary(
+                        mCustomLogoStyle.getEntries()[index]);
             return true;
 	}
         return false;
