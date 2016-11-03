46c2843f0eb86c5a2531f6ef1f409e219be9dfec
Lars Greiss <kufikugel@googlemail.com>
2016-11-03 08:53:49 +0800

    Settings: HeadsUp: add timeout option [2/2]
    Change-Id: Ic28f123e9705af45ee0a43c7eddcaf78ab0227cb

--- src/com/android/settings/notification/ConfigureNotificationSettings.java
+++ src/com/android/settings/notification/ConfigureNotificationSettings.java
@@ -20,6 +20,7 @@ import android.app.admin.DevicePolicyManager;
 import android.app.NotificationManager;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.net.Uri;
 import android.os.Bundle;
@@ -27,6 +28,7 @@ import android.os.Handler;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.Settings;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
@@ -46,13 +48,15 @@ import static android.app.admin.DevicePolicyManager.KEYGUARD_DISABLE_SECURE_NOTI
 import static android.app.admin.DevicePolicyManager.KEYGUARD_DISABLE_UNREDACTED_NOTIFICATIONS;
 import static com.android.settingslib.RestrictedLockUtils.EnforcedAdmin;
 
-public class ConfigureNotificationSettings extends SettingsPreferenceFragment {
+public class ConfigureNotificationSettings extends SettingsPreferenceFragment
+        implements OnPreferenceChangeListener {
     private static final String TAG = "ConfigNotiSettings";
 
     private static final String KEY_NOTIFICATION_PULSE = "notification_pulse";
     private static final String KEY_LOCK_SCREEN_NOTIFICATIONS = "lock_screen_notifications";
     private static final String KEY_LOCK_SCREEN_PROFILE_NOTIFICATIONS =
             "lock_screen_notifications_profile";
+    private static final String PREF_HEADS_UP_TIME_OUT = "heads_up_time_out";
 
     private final SettingsObserver mSettingsObserver = new SettingsObserver();
 
@@ -66,6 +70,7 @@ public class ConfigureNotificationSettings extends SettingsPreferenceFragment {
     private int mLockscreenSelectedValue;
     private int mLockscreenSelectedValueProfile;
     private int mProfileChallengeUserId;
+    private ListPreference mHeadsUpTimeOut;
 
     @Override
     protected int getMetricsCategory() {
@@ -96,6 +101,22 @@ public class ConfigureNotificationSettings extends SettingsPreferenceFragment {
             addPreferencesFromResource(R.xml.configure_notification_settings_profile);
             initLockscreenNotificationsForProfile();
         }
+
+        Resources systemUiResources;
+        try {
+            systemUiResources = getPackageManager().getResourcesForApplication("com.android.systemui");
+        } catch (Exception e) {
+            return;
+        }
+
+        int defaultTimeOut = systemUiResources.getInteger(systemUiResources.getIdentifier(
+                    "com.android.systemui:integer/heads_up_notification_decay", null, null));
+        mHeadsUpTimeOut = (ListPreference) findPreference(PREF_HEADS_UP_TIME_OUT);
+        mHeadsUpTimeOut.setOnPreferenceChangeListener(this);
+        int headsUpTimeOut = Settings.System.getInt(getContentResolver(),
+                Settings.System.HEADS_UP_TIMEOUT, defaultTimeOut);
+        mHeadsUpTimeOut.setValue(String.valueOf(headsUpTimeOut));
+        updateHeadsUpTimeOutSummary(headsUpTimeOut);
 
     }
 
@@ -381,5 +402,24 @@ public class ConfigureNotificationSettings extends SettingsPreferenceFragment {
                 }
             }
         }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mHeadsUpTimeOut) {
+            int headsUpTimeOut = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.HEADS_UP_TIMEOUT,
+                    headsUpTimeOut);
+            updateHeadsUpTimeOutSummary(headsUpTimeOut);
+            return true;
+        }
+        return false;
+    }
+
+    private void updateHeadsUpTimeOutSummary(int value) {
+        String summary = getResources().getString(R.string.heads_up_time_out_summary,
+                value / 1000);
+        mHeadsUpTimeOut.setSummary(summary);
     }
 }
